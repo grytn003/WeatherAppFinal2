7c2e6e08baf24f75b3bd1d8b5211526cdd04bfcd
grytn003 <grytn003@cougars.csusm.edu>
2017-12-15 02:38:36 -0800

    added basic visual style

--- Week9PrismExampleApp/ApiKey.cs
+++ Week9PrismExampleApp/ApiKey.cs
@@ -4,6 +4,6 @@ namespace Week9PrismExampleApp
     public class ApiKey
     {
 		// Obtain API Key https://openweathermap.org/api
-		public const string WeatherAPIKey = "0d3a5da3f6788bc487d9bbf040a3024e";
+		public const string WeatherAPIKey = "";
     }
 }
